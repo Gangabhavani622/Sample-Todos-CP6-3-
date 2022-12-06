2089cf29488a5183ecfc6207da7764939037ba8b
Gangabhavani622 <gangabhavani0628@gmail.com>
2022-12-06 08:24:38 +0000

    Simple TodosList CP6(3)

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,22 @@
+// Write your code her
+const TodoItem = props => {
+  const {todoItem, deleteTodo} = props
+  const {id, title} = todoItem
+
+  onDeleteItem = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    <li>
+      <div className="list-style">
+        <p className="title">{title}</p>
+        <button type="button" className="button" onClick={onDeleteItem()}>
+          Delete
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default TodoItem
